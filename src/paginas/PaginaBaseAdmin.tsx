e8bc97033915a2c0561b8fe52212e670e790f98f
bobokamura <roberto.okamura@cotefacil.com>
2022-12-02 09:49:58 -0300

    feat: posicionamento do home

--- src/paginas/PaginaBaseAdmin.tsx
+++ src/paginas/PaginaBaseAdmin.tsx
@@ -18,28 +18,24 @@ const PaginaBaseAdmin = () => {
           <Toolbar>
             <Typography variant="h6">Administração</Typography>
             <Box sx={{ display: "flex", flexGrow: 1 }}>
+              <Link component={RouterLink} to="/restaurantes">
+                <Button sx={{ my: 2, color: "white" }}>Home</Button>
+              </Link>
+
               <Link component={RouterLink} to="/admin/restaurantes">
                 <Button sx={{ my: 2, color: "white" }}>Restaurantes</Button>
               </Link>
+
               <Link component={RouterLink} to="/admin/restaurantes/novo">
-                <Button sx={{ my: 2, color: "white" }}>
-                  Novo Restaurante
-                </Button>
+                <Button sx={{ my: 2, color: "white" }}>Novo Restaurante</Button>
               </Link>
+
               <Link component={RouterLink} to="/admin/pratos">
-                <Button sx={{ my: 2, color: "white" }}>
-                  Pratos
-                </Button>
+                <Button sx={{ my: 2, color: "white" }}>Pratos</Button>
               </Link>
+
               <Link component={RouterLink} to="/admin/pratos/novo">
-                <Button sx={{ my: 2, color: "white" }}>
-                  Novo Prato
-                </Button>
-              </Link>
-              <Link component={RouterLink} to="/restaurantes">
-                <Button sx={{ my: 2, color: "white", position: 'absolute' }}>
-                  Home
-                </Button>
+                <Button sx={{ my: 2, color: "white" }}>Novo Prato</Button>
               </Link>
             </Box>
           </Toolbar>
@@ -50,7 +46,7 @@ const PaginaBaseAdmin = () => {
         <Container maxWidth="lg" sx={{ mt: 1 }}>
           <Paper sx={{ p: 2 }}>
             {/*Conteúdo da Página*/}
-            <Outlet/>
+            <Outlet />
           </Paper>
         </Container>
       </Box>
